1c427bb27c345d07822d7ce1a83fd7d4d04fe904
Raphael V. Coelho <158857536+RaphaZY@users.noreply.github.com>
2024-03-17 18:04:33 -0300

    Update2.0 calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -14,7 +14,7 @@ class Calculadora:
         return n1 * n2
     
     def divi(self, n1:int, n2:int):
-        if n2 == 0:
+        if n2 == 0 or n1 == 0:
             return "Erro: divisão por zero."
         return n1 / n2
     
@@ -27,14 +27,48 @@ class Calculadora:
     def potenciacao(self, base:int, expoente:int):
         return base ** expoente
 
-
+operacao = 1
 calc = Calculadora()
 
-print("Soma:", calc.soma(500, 478))              
-print("Subtração:", calc.sub(1, -100))     
-print("Multiplicação:", calc.mult(75, 4))  
-print("Divisão:", calc.divi(5, 3))        
-print("Divisão por zero:", calc.divi(1, 0)) 
-print("Porcentagem:", calc.porcent(1000, 70))  
-print("Raiz Quadrada:", calc.raiz_quadrada(121))   
-print("Potenciação:", calc.potenciacao(8, 3))     
+while operacao != 0: 
+    operacao = input("Digite a operação desejada (+, -, *, /, %, v, ^) ou digite 0 para terminar: ")
+
+    if operacao == "0":
+        print("Fim do programa")
+        break
+    elif operacao == '+':
+        num1 = float(input("Digite o primeiro número: "))
+        num2 = float(input("Digite o segundo número: "))
+        resultado = calc.soma(num1, num2)
+        print(resultado)
+    elif operacao == '-':
+        num1 = float(input("Digite o primeiro número: "))
+        num2 = float(input("Digite o segundo número: "))
+        resultado = calc.sub(num1, num2)
+        print(resultado)
+    elif operacao == '*':
+        num1 = float(input("Digite o primeiro número: "))
+        num2 = float(input("Digite o segundo número: "))
+        resultado = calc.mult(num1, num2)
+        print(resultado)
+    elif operacao == '/':
+        num1 = float(input("Digite o primeiro número: "))
+        num2 = float(input("Digite o segundo número: "))
+        resultado = calc.divi(num1, num2)
+        print(resultado)
+    elif operacao == '%':
+        num1 = float(input("Digite o primeiro número: "))
+        num2 = float(input("Digite o segundo número: "))
+        resultado = calc.porcent(num1, num2)
+        print(resultado)
+    elif operacao == 'v':
+        num1 = float(input("Digite o primeiro número: "))
+        resultado = calc.raiz_quadrada(num1)
+        print(resultado)
+    elif operacao == '^':
+        num1 = float(input("Digite o primeiro número: "))
+        num2 = float(input("Digite o segundo número: "))
+        resultado = calc.potenciacao(num1, num2)
+        print(resultado)
+    else:
+        print("Operação inválida!")
